af127d131e58793743846c1d5f6d6e2f4f7b4c6d
Muhammad Saqib <saqibtechbraniz562@gmail.com>
2023-01-07 13:33:38 -0600

    added url parameter to useFetch hook

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Rows from "./components/Rows";
 import useFetch from "./Redux/hooks/useFetch";
 
 function App() {
-  const { data, twoDimensionalArray } = useFetch();
+  const { data, twoDimensionalArray } = useFetch("http://localhost:9876/init");
   let rowCounter = 0;
 
   return (
--- src/Redux/hooks/useFetch.ts
+++ src/Redux/hooks/useFetch.ts
@@ -2,10 +2,10 @@ import { useEffect } from "react";
 import { getData } from "../features/boxesSlice";
 import { useAppDispatch, useAppSelector } from "./useApp";
 
-const useFetch = () => {
+const useFetch = (url: string) => {
   const dispatch = useAppDispatch();
   useEffect(() => {
-    dispatch(getData("http://localhost:9876/init"));
+    dispatch(getData(url));
   }, []);
 
   const { data, difference, twoDimensionalArray, movesLeft } = useAppSelector((state) => state.boxes);
@@ -14,16 +14,16 @@ const useFetch = () => {
     setTimeout(() => {
       if (difference < 10 && difference !== 0) {
         if (window.confirm("Success!. Do You Want to replay")) {
-          return dispatch(getData(`http://localhost:9876/init/user/${data.userId}`));
+          return dispatch(getData(`${url}/user/${data.userId}`));
         }
-        return dispatch(getData("http://localhost:9876/init"));
+        return dispatch(getData(url));
       }
 
       if (movesLeft === 0) {
         if (window.confirm("Failed!. Do You Want to replay")) {
-          return dispatch(getData(`http://localhost:9876/init/user/${data.userId}`));
+          return dispatch(getData(`${url}/user/${data.userId}`));
         }
-        return dispatch(getData("http://localhost:9876/init"));
+        return dispatch(getData(url));
       }
     }, 100);
   }, [difference, movesLeft]);
